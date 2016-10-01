aa52bed09893435140dbe1e809555091b1abbb82
Daniel Wegner <dwegner@neoteric.eu>
2016-10-01 18:34:24 +0200

    dev15 mavenized

---
+++ src/main/java/uk/co/eluinhost/UltraHardcore/commands/inter/UHCCommand.java
@@ -0,0 +1,8 @@
+package uk.co.eluinhost.UltraHardcore.commands.inter;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabExecutor;
+
+public abstract class UHCCommand implements CommandExecutor,TabExecutor{
+
+}
